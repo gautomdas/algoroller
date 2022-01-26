2380d2d073a5eb56e8b653ac22c5395e874c289e
Gautom Das <gautomdas4@gmail.com>
2022-01-25 20:34:08 -0500

    avoid crypto dependency error

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,37 @@ import { GradientText } from "./components/Gradient";
 import Input from "./components/Input";
 import Navbar from "./components/Navbar";
 import Roulette from "./components/Roulette";
+import { algodClient, connection } from "./utils/connection";
+import { Accounts } from "@randlabs/myalgo-connect";
+import algosdk from "algosdk";
+
+import { Fragment, useEffect, useState } from "react";
+
+let timeoutResolution: NodeJS.Timeout | null = null;
 
 function App() {
+  const [params, setParams] = useState<algosdk.SuggestedParams>();
+  const [accounts, setAccounts] = useState<Accounts[]>([]);
+
+  const onCompleteConnect = (accounts: Accounts[]): void => {
+    setAccounts(accounts);
+  };
+
+  const getTransactionParams = async (): Promise<void> => {
+    try {
+      const params = await algodClient.getTransactionParams().do();
+      setParams(params);
+    } catch (err) {
+      console.error(err);
+    }
+    timeoutResolution = setTimeout(getTransactionParams, 10000);
+  };
+
+  useEffect(() => {
+    if (timeoutResolution) clearTimeout(timeoutResolution);
+    getTransactionParams();
+  }, [accounts]);
+
   return (
     <div className="App bg-navy-500 flex flex-col h-screen px-10 lg:px-32 xl:px-64 text-white">
       <header className="App-header">
